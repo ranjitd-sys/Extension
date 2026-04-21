ce83d7893dfdd6412faafbfb0c33862561b9a430
ranjit <ranjitd-sys>
2026-04-21 16:32:48 +0530

    added some thsing

--- background.js
+++ background.js
@@ -1,2 +1,15 @@
-// background.js
-console.log("Background running");
+const socket = new WebSocket("ws://localhost:8080");
+
+socket.onopen = () => {
+  console.log("Connected to external program");
+};
+
+socket.onmessage = (event) => {
+  const msg = JSON.parse(event.data);
+  console.log("Received:", msg);
+};
+
+// send back if needed
+function send(msg) {
+  socket.send(JSON.stringify(msg));
+}
